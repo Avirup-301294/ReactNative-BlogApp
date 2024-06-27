19c352a5180dd41c8ce5981187b8d8bddde702bc
Mondal <avirupmondal1994@gmail.com>
2024-06-27 12:39:58 +0530

    added create screen

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,23 +1,36 @@
-import React, { useState, useReducer } from "react";
-import CreateDataContext from "./CreateDataContext";
+import createDataContext from "./createDataContext";
 
 const blogReducer = (state, action) => {
   switch (action.type) {
     case "add_blogpost":
-      return [...state, { title: `BlogPost #${state.length + 1}` }];
+      return [
+        ...state,
+        {
+          id: Math.floor(Math.random() * 99999),
+          title: `BlogPost #${state.length + 1}`,
+        },
+      ];
+    case "delete_blogpost":
+      return state.filter((blogPost) => blogPost.id !== action.payload);
     default:
       return state;
   }
 };
 
-const addBlogPost = ( dispatch ) => {
-    return () => {
-        dispatch({ type: "add_blogpost" });
-    }
+const addBlogPost = (dispatch) => {
+  return () => {
+    dispatch({ type: "add_blogpost" });
+  };
 };
 
-export const { Context, Provider } = CreateDataContext(
+const deleteBlogPost = (dispatch) => {
+  return (id) => {
+    dispatch({ type: "delete_blogpost", payload: id });
+  };
+};
+
+export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost },
+  { addBlogPost, deleteBlogPost },
   []
 );
